00ec0cacf669b91c5cd25533b725e04afc05a6e0
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2021-07-15 20:45:16 +0200

    feat: line-chart exercise

--- api-server.js
+++ api-server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const SimplexNoise = require('simplex-noise')
 
 const app = express()
 
@@ -42,6 +43,21 @@ app.get('/colors', (req, res) => {
     value: minAmount + Math.round(Math.random() * maxAmount)
   }))
   res.json(data)
+})
+
+app.get('/stocks', (req, res) => {
+  const pointsCount = 50
+  const simplex = new SimplexNoise()
+  const todayDate = new Date()
+  const arr = new Array(pointsCount).fill(null).map((_, i) => {
+    const date = new Date()
+    date.setDate(todayDate.getDate() - pointsCount + i + 1)
+    return {
+      date,
+      value: (simplex.noise2D(i * 0.08, i * 0.01) + 1) * 500
+    }
+  })
+  res.json(arr)
 })
 
 app.listen(PORT, () => console.log('\x1b[33m%s\x1b[0m', `\nAPI is listening on: http://localhost:${PORT}\n\n`))
